502681c53e106facd12b0e43577f21ed73b98d9f
Vengineer <vengineer@preferred.jp>
2018-09-04 10:25:40 +0900

    test on Android

---
+++ build_android_aarch64.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+CXX=aarch64-linux-android-clang++ \
+ CC=aarch64-linux-android-clang \
+ LD=aarch64-linux-android-ld \
+ cmake -DENABLE_ANDROID_AARCH64=ON -DENABLE_ARMNN_X86_64=OFF ..
+
+# CXX_FLAGS="-fPIE -fPIC" \
+
