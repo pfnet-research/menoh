fe8ae8f2dec836521e1ef7379f532ac1fd21a6cd
okdshin <kokuzen@gmail.com>
2018-10-29 19:25:29 +0900

    fix error

--- menoh/mkldnn_with_generic_fallback/backend/generic/operator/mul.hpp
+++ menoh/mkldnn_with_generic_fallback/backend/generic/operator/mul.hpp
@@ -2,12 +2,13 @@
 #define MENOH_IMPL_MKLDNN_WITH_GENERIC_FALLBACK_BACKEND_BACKEND_GENERIC_OPERATOR_MUL_HPP
 
 #include <menoh/array.hpp>
+#include <menoh/graph.hpp> // for dimension_mismatch error
 #include <menoh/mkldnn_with_generic_fallback/procedure.hpp>
 
 namespace menoh_impl {
     namespace mkldnn_with_generic_fallback_backend {
         namespace generic_backend {
-            inline procedure make_mul(node const&,
+            inline procedure make_mul(node const& node,
                                       std::vector<array> const& input_list,
                                       std::vector<array> const& output_list) {
                 assert(input_list.size() == 2);
@@ -15,11 +16,17 @@ namespace menoh_impl {
 
                 for(auto const& input : input_list) {
                     if(input.dtype() != dtype_t::float_) {
-                        throw std::runtime_error("invalid dtype");
+                        throw invalid_dtype(
+                          std::to_string(static_cast<int>(input.dtype())));
                     }
                 }
-                if(total_size(input_list.at(0)) != total_size(input_list.at(1))) {
-                    throw std::runtime_error("broadcast is not supported yet");
+                if(total_size(input_list.at(0)) !=
+                   total_size(input_list.at(1))) {
+                    throw dimension_mismatch(
+                      node.op_type, node.output_name_list.front(),
+                      "total size is invalid. broadcast is not supported yet",
+                      std::to_string(total_size(input_list.at(0))),
+                      std::to_string(total_size(input_list.at(1))));
                 }
 
                 auto procedure = [input_a = input_list.at(0),
