e4a790874ec4de4a4f8f58318f386bacd36ff77d
okdshin <kokuzen@gmail.com>
2018-10-29 18:08:38 +0900

    add ResNet50 example

--- scripts/retrieve_data.py
+++ scripts/retrieve_data.py
@@ -14,6 +14,7 @@ def wget( address, target ):
         urllib.request.urlretrieve(address,  target)
 
 wget('https://www.dropbox.com/s/bjfn9kehukpbmcm/VGG16.onnx?dl=1', './data/VGG16.onnx')
+wget('https://preferredjp.box.com/shared/static/x4k1pya1w1vuvpfk77pp6ma8oaye8syb.onnx', './data/resnet50.onnx')
 wget('https://raw.githubusercontent.com/HoldenCaulfieldRye/caffe/master/data/ilsvrc12/synset_words.txt', './data/synset_words.txt')
 wget('https://upload.wikimedia.org/wikipedia/commons/5/54/Light_sussex_hen.jpg', './data/Light_sussex_hen.jpg')
 
