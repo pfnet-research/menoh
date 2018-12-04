df144a046c5c70ae874c99b0958bf25ad418f893
Vengineer <vengineer@preferred.jp>
2018-12-04 10:44:50 +0900

    rename Util.hpp to GraphTopologicalSort.hpp

--- menoh/tensorrt/GraphTopologicalSort.hpp
+++ menoh/tensorrt/GraphTopologicalSort.hpp
@@ -1,10 +1,16 @@
+//
+// Copyright © 2017 Arm Ltd. All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
 #pragma once
+
+#include <boost/assert.hpp>
 
 #include <functional>
 #include <map>
 #include <vector>
 
-namespace Util
+namespace armnnUtils
 {
 
 namespace
@@ -36,7 +42,7 @@ bool Visit(
         }
         else
         {
-            assert(0);
+            BOOST_ASSERT(false);
         }
     }
 
@@ -55,6 +61,12 @@ bool Visit(
 
 }
 
+// Sorts an directed acyclic graph (DAG) into a flat list such that all inputs to a node are before the node itself.
+// Returns true if successful or false if there is an error in the graph structure (e.g. it contains a cycle).
+// The graph is defined entirely by the "getIncomingEdges" function which the user provides. For a given node,
+// it must return the list of nodes which are required to come before it.
+// "targetNodes" is the list of nodes where the search begins - i.e. the nodes that you want to evaluate.
+// The implementation is based on https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
 template<typename TNodeId, typename TTargetNodes>
 bool GraphTopologicalSort(
     const TTargetNodes& targetNodes,
@@ -76,3 +88,4 @@ bool GraphTopologicalSort(
 }
 
 }
+
