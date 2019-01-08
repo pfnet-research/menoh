825afb7e4a3075800d27ef9854b69d57e33bb0bd
okdshin <kokuzen@gmail.com>
2019-01-08 03:40:36 +0000

    remove wrong comments

--- test/operator.cpp
+++ test/operator.cpp
@@ -399,10 +399,10 @@ namespace {
     TEST_OP_STATIC_PARAMS(tensorrt, test_mul_example, eps);
   
     // Softmax
-    TEST_OP_STATIC_PARAMS(tensorrt, test_softmax_axis_0, eps); // fails
-    TEST_OP_STATIC_PARAMS(tensorrt, test_softmax_axis_1, eps); // fails
-    TEST_OP_STATIC_PARAMS(tensorrt, test_softmax_axis_2, eps); // fails
-    TEST_OP_STATIC_PARAMS(tensorrt, test_softmax_default_axis, eps); // fails
+    TEST_OP_STATIC_PARAMS(tensorrt, test_softmax_axis_0, eps);
+    TEST_OP_STATIC_PARAMS(tensorrt, test_softmax_axis_1, eps);
+    TEST_OP_STATIC_PARAMS(tensorrt, test_softmax_axis_2, eps);
+    TEST_OP_STATIC_PARAMS(tensorrt, test_softmax_default_axis, eps);
     TEST_OP_STATIC_PARAMS(tensorrt, test_softmax_example, eps);
     TEST_OP_STATIC_PARAMS(tensorrt, test_softmax_large_number, eps);
 
