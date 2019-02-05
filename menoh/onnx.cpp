59f8d760ec0eddffac893d8161c33396972f62ab
okdshin <kokuzen@gmail.com>
2019-02-05 14:29:05 +0900

    replace including onnx.pb.h onnx_pb.h
    latter defines ONNX_API

--- menoh/onnx.cpp
+++ menoh/onnx.cpp
@@ -11,7 +11,7 @@
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 
-#include <onnx/onnx.pb.h>
+#include <onnx/onnx_pb.h>
 
 #include <menoh/array.hpp>
 #include <menoh/exception.hpp>
