f8aebde768763e80c024d1ea4403d586a5916817
okdshin <kokuzen@gmail.com>
2019-03-14 18:26:02 +0900

    fix bug

--- menoh/dtype.hpp
+++ menoh/dtype.hpp
@@ -85,30 +85,7 @@ namespace menoh_impl {
     template <dtype_t d>
     constexpr int size_in_bytes = sizeof(dtype_to_type_t<d>);
 
-    int get_size_in_bytes(dtype_t d) {
-        if(d == dtype_t::float16) {
-            return size_in_bytes<dtype_t::float16>;
-        } else
-        if(d == dtype_t::float32) {
-            return size_in_bytes<dtype_t::float32>;
-        }else
-        if(d == dtype_t::float64) {
-            return size_in_bytes<dtype_t::float64>;
-        } else
-        if(d == dtype_t::int8) {
-            return size_in_bytes<dtype_t::int8>;
-        } else
-        if(d == dtype_t::int16) {
-            return size_in_bytes<dtype_t::int16>;
-        } else
-        if(d == dtype_t::int32) {
-            return size_in_bytes<dtype_t::int32>;
-        } else
-        if(d == dtype_t::int64) {
-            return size_in_bytes<dtype_t::int64>;
-        }
-        throw invalid_dtype(std::to_string(static_cast<int>(dtype)));
-    }
+    int get_size_in_bytes(dtype_t d);
 
 } // namespace menoh_impl
 
