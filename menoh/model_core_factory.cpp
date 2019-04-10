bbb3b46ba1b147d9b244e1ce41f3128f7d5672ee
Harunobu Miyashita <vengineer@preferred.jp>
2019-04-10 11:56:53 +0900

    merge master

--- menoh/model_core_factory.cpp
+++ menoh/model_core_factory.cpp
@@ -1,7 +1,6 @@
 #include <menoh/model_core.hpp>
 #include <menoh/model_core_factory.hpp>
 
-<<<<<<< HEAD
 #if ENABLE_MKLDNN
 #include <menoh/mkldnn/model_core.hpp>
 #endif
@@ -15,7 +14,7 @@
 
 namespace menoh_impl {
 
-#if ENABLE_MKLDNN
+
     std::unique_ptr<menoh_impl::model_core> make_model_core(
       std::unordered_map<std::string, array> const& input_table,
       std::unordered_map<std::string, array> const& required_output_table,
@@ -23,6 +22,7 @@ namespace menoh_impl {
         output_profile_table,
       menoh_impl::model_data const& model_data, std::string const& backend_name,
       backend_config const& config) {
+#if ENABLE_MKLDNN
         if(backend_name == "mkldnn") {
             return std::make_unique<mkldnn_backend::model_core>(
               mkldnn_backend::make_model_core(
@@ -42,7 +42,6 @@ namespace menoh_impl {
                 model_data, config));
         }
 #endif
-
 #if ENABLE_ARMNN
         if(backend_name == "armnn") {
             return std::make_unique<armnn_backend::model_core>(
@@ -50,8 +49,6 @@ namespace menoh_impl {
                                               model_data, config));
         }
 #endif
-
         throw invalid_backend_name(backend_name);
     }
-
 } // namespace menoh_impl
